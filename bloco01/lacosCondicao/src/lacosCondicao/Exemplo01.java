9ea7f417039b151ccdaa78adbcbe488f826d0b32
nathannolacio <nathannolacio04@gmail.com>
2023-12-08 13:53:44 -0300

    Subindo arquivos sobre laçoes de condição

---
+++ bloco01/lacosCondicao/src/lacosCondicao/Exemplo01.java
@@ -0,0 +1,19 @@
+package lacosCondicao;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+		
+		boolean x = true;
+		boolean y = true;
+		
+		if(x == true) {
+			System.out.println("X é verdadeiro");
+		}
+		
+		if(y) {
+			System.out.println("Y é verdadeiro");
+		}
+	}
+
+}
---
+++ bloco01/lacosCondicao/src/lacosCondicao/Exemplo06.java
@@ -0,0 +1,35 @@
+package lacosCondicao;
+
+import java.util.Scanner;
+
+public class Exemplo06 {
+
+	public static void main(String[] args) {
+		
+		int opcao;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("* ** *** **** ***** MENU ***** **** *** ** *");
+		System.out.println("\nDigite 1 para ver uma indicação de livro");
+		System.out.println("Digite 2 para ler uma frase motivacional");
+		System.out.println("Digite 3 para receber uma indicação");
+		
+		opcao = leia.nextInt();
+		leia.close();	
+		
+		switch (opcao) {
+		case 1:
+			System.out.println("Livro: Harry Potter e a Pedra Filosofal");
+		break;
+		
+		case 2:
+			System.out.println("Frase Motivacional: Por mais fundo que seja o buraco, sorria. Afinal, ainda não tem terra caindo na sua cabeça");
+		break;
+		
+		case 3:
+			System.out.println("Música: Bless me - Kirk Franklin ft. Maverick City");
+		}
+	}
+
+}
